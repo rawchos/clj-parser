f3c5a7f49b4def557f248bb14aceca64ef69a9f7
Neal Scott <nealnmel@gmail.com>
2021-03-10 13:27:29 -0600

    Update POST endpoint to accept json instead of string to parse

--- src/api/core.clj
+++ src/api/core.clj
@@ -6,6 +6,7 @@
             [camel-snake-kebab.core :refer [->camelCase
                                             ->kebab-case]]
             [clojure.string :refer [lower-case]]
+            [clojure.walk :refer [postwalk]]
             [compojure.api.sweet :refer [api
                                          context
                                          GET
@@ -27,7 +28,7 @@
   []
   @records)
 
-(defn add-record [{data-line :data}]
+(defn old-add-record [{data-line :data}]
   (let [record (try
                  (util/parse-record data-line)
                  (catch Exception _ (throw-ex! {:type :bad-request
@@ -41,16 +42,25 @@
       (swap! records conj record))
     (lower-case (:last-name record))))
 
-(defn convert-date [records]
-  (map
-   #(reduce-kv
-     (fn [m k v]
-       (if (= :birth-date k)
-         (assoc m k (util/date->string v))
-         (assoc m k v)))
-     {}
-     %)
-   records))
+
+(defn add-record [record]
+  (if ((set (get-records)) record)
+    (throw-ex! {:type :conflict
+                :title "Record Already Exists"
+                :message "A record for this person already exists"
+                :error-data record})
+    (swap! records conj record))
+  (lower-case (:last-name record)))
+
+(defn update-record [{:keys [birth-date] :as record} convert-fn]
+  (assoc record :birth-date (convert-fn birth-date)))
+
+(defn convert-date [records convert-fn]
+   (postwalk (fn [x]
+               (if (map? x)
+                 (update-record x convert-fn)
+                 x))
+             records))
 
 (defn encode-key [k]
   (->camelCase (name k)))
@@ -82,30 +92,31 @@
      :tags ["api"]
 
      (POST "/records" []
-       :body [record ParseRecord]
+       :body [record Record]
        :summary "Creates a new Record if it doesn't already exist."
-       (created (str "/api/records/" (add-record record))))
+       (created (str "/api/records/" (add-record (convert-date record
+                                                               util/string->date)))))
      
      (GET "/records/email" []
        :return {:data [Record]}
        :summary "Returns a list of Records sorted by email descending and last name ascending"
        (ok {:data (-> (get-records)
                       (util/sort-email-last-name)
-                      (convert-date))}))
+                      (convert-date util/date->string))}))
 
      (GET "/records/birthdate" []
        :return {:data [Record]}
        :summary "Returns a list of Records sorted by birthdate ascending"
        (ok {:data (-> (get-records)
                       (util/sort-birth-date)
-                      (convert-date))}))
+                      (convert-date util/date->string))}))
      
      (GET "/records/name" []
        :return {:data [Record]}
        :summary "Returns a list of Records sorted by lastname descending"
        (ok {:data (-> (get-records)
                       (util/sort-last-name)
-                      (convert-date))}))
+                      (convert-date util/date->string))}))
 
      (GET "/server/status" []
        :return {:data schema/Str}
